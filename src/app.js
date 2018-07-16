64c05a11198650ba4835556d787363eead98d498
Storm4542 <417002107@qq.com>
2018-07-17 00:12:06 +0800

    npm test && npm dev-test

--- src/app.js
+++ src/app.js
@@ -16,3 +16,22 @@ new Vue({
 });
 
 //单元测试
+import chai from 'chai'
+import charSpies from 'chai-spies'
+
+const expect = chai.expect;
+
+{
+    const Construstor = Vue.extend(Button);
+    const vm = new Construstor({
+        propsData:{
+            icon : 'setting'
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-setting');
+    vm.$el.remove();
+    vm.$destroy();
+}
