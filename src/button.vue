04003107e89d53bc2208c3d468abe2f4c5eb57f6
Storm4542 <417002107@qq.com>
2018-07-16 10:43:15 +0800

    parcel

---
+++ src/button.vue
@@ -0,0 +1,35 @@
+<template>
+
+       <button class="g-button">按钮</button>
+
+</template>
+
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background: var(--button-bg);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        color: var(--color);
+        &:hover{
+            border-color: var(--border-hover-bg);
+        }
+
+        &:active{
+            background: var(--button-active-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+
+
+</style>
