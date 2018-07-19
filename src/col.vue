66a363d1cae82653e13247c502a82e53849d88bf
Storm4542 <417002107@qq.com>
2018-07-19 22:04:24 +0800

    row 和 col 的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="`col-${span}`" ><slot/></div>
+</template>
+
+<script>
+    export default {
+        name: 'g-col',
+        props:{
+            span:{
+                type:[Number,String]
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    .col{
+        height: 100px;
+        border: 1px solid grey;
+        background: green;
+        width: 50%;
+    }
+    .generate-columns(@n, @i: 1) when (@i =< @n) {
+        .col-@{i} {
+            width: (@i * 100% / @n);
+        }
+        .generate-columns(@n, (@i + 1));
+    }
+    .generate-columns(24);
+</style>
---
+++ src/row.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row"><slot/></div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="less" scoped>
+    .row{display: flex}
+</style>
