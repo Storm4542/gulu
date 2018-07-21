658089883dba3624dbee969bb75072e1b9a16d07
Storm4542 <417002107@qq.com>
2018-07-21 13:57:24 +0800

    布局组件基本功能完成

--- src/layout.vue
+++ src/layout.vue
@@ -1,19 +1,40 @@
 <template>
-    <div class="layout">
+    <div class="layout" :class="layoutClass">
         <slot/>
     </div>
 </template>
 
 <script>
     export default {
-        name: "layout"
+        name: "g-layout",
+        data(){
+            return{
+                layoutClass:{
+                    hasSider:false
+                }
+            }
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                console.log(vm.$options.name);
+                if(vm.$options.name === 'g-sider'){
+                    this.layoutClass.hasSider = true
+                }
+            })
+        }
     }
 </script>
 
 <style lang="less" scoped>
     .layout{
         display: flex;
+        flex-grow: 1;
         flex-direction: column;
         border: 1px solid red;
+        &.hasSider{
+            flex-direction: row;
+        }
     }
+
+
 </style>
--- src/sider.vue
+++ src/sider.vue
@@ -6,7 +6,7 @@
 
 <script>
     export default {
-        name: "sider"
+        name: "g-sider"
     }
 </script>
 
