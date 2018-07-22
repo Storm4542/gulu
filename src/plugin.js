7ab03c0116d157b80a8ac1cc911652433737aa6a
Storm4542 <417002107@qq.com>
2018-07-22 19:23:42 +0800

    实现了页面只能有一个toast的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,21 @@
 import Toast from './toast'
-
+let currentToast;
 export default {
-    install(vue, options) {
+    install(vue) {
         vue.prototype.$toast = function (message,toastOptions) {
-            let construstor = vue.extend(Toast);
-            let toast = new construstor({
-                propsData:toastOptions
-                });
-            toast.$slots.default = [message];//传输默认插槽的内容 <slot/>
-            toast.$mount(); //mount一下
-            document.body.appendChild(toast.$el) //
+            if(currentToast){
+                currentToast.close();
+            }
+          currentToast = createToast({vue,message,propsData:toastOptions});
         }
     }
+}
+/*helpers*/
+function createToast({vue,message,propsData}) {
+    let construstor = vue.extend(Toast);
+    let toast = new construstor({propsData});
+    toast.$slots.default = [message];//传输默认插槽的内容 <slot/>
+    toast.$mount(); //mount一下
+    document.body.appendChild(toast.$el) //
+    return toast;
 }
