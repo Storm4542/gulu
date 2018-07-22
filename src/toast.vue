74c69e5441d1b71b3fff4ec6841f084adf405d8b
Storm4542 <417002107@qq.com>
2018-07-22 23:05:31 +0800

    M

--- src/toast.vue
+++ src/toast.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="wrapper">
         <div ref="toast" class="toast" :class="toastClasses">
             <div class="message">
                 <slot v-if="!enableHtml"></slot>
@@ -9,6 +10,7 @@
             {{closeButton.text}}
         </span>
         </div>
+    </div>
 </template>
 
 <script>
@@ -83,25 +85,64 @@
     @toast-fontcolor: white;
 
     @keyframes fade-in {
-        0%{opacity: 0;transform: translateY(100%)}
-        100%{opacity: 1;transform: translateY(0)}
+        0% {
+            opacity: 0;
+            transform: translateY(100%)
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0)
+        }
     }
+    .wrapper{
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+
+    }
+
     .toast {
         animation: fade-in 1s;
-        position: fixed;left: 50%;min-height: @toast-minHeight;
-        font-size: @font-size;color: @toast-fontcolor;line-height: 1.8;
-        padding: 0 .5em 0 1.5em;background: @toast-bg;display: flex;
-        align-items: center;border-radius: 4px;box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
-        &.message {padding: 0.5em 0}
-        &.position-top {top: 0;transform: translateX(-50%);}
-        &.position-bottom {bottom: 0;transform: translateX(-50%);}
-        &.position-middle {top: 50%;transform: translate(-50%, -50%);}
-        .line {border: 1px solid white;height: 100%;margin-left: 1em;}
-        .close {padding-left: 1em;flex-shrink: 0;}
+
+        min-height: @toast-minHeight;
+        font-size: @font-size;
+        color: @toast-fontcolor;
+        line-height: 1.8;
+        padding: 0 .5em 0 1.5em;
+        background: @toast-bg;
+        display: flex;
+        align-items: center;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+        &.message {
+            padding: 0.5em 0
+        }
+        &.position-top {
+            top: 0;
+            transform: translateX(-50%);
+        }
+        &.position-bottom {
+            bottom: 0;
+        }
+        &.position-middle {
+            top: 50%;
+            transform: translate(-50%, -50%);
+        }
+        .line {
+            border: 1px solid white;
+            height: 100%;
+            margin-left: 1em;
+        }
+        .close {
+            padding-left: 1em;
+            flex-shrink: 0;
+        }
     }
+
     .fade-enter-active, .fade-leave-active {
         transition: opacity .5s;
     }
+
     .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
         opacity: 0;
     }
