0e58e876d87b7e96106af15bc855aed8eb0c3806
Storm4542 <417002107@qq.com>
2018-07-29 16:01:47 +0800

    解决当容器有overflow造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,48 +1,54 @@
 <template>
     <div class="popover" @click="xxx()">
-        <div class="content-wrapper" @click.stop v-if="visible">
-            <slot  name="content" ></slot>
+        <div ref="contentWrapper" class="content-wrapper" @click.stop v-if="visible">
+            <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
         name: "g-popover",
-        data(){
-            return{
-                visible:false
+        data() {
+            return {
+                visible: false
             }
         },
-        methods:{
-            xxx(){
+        methods: {
+            xxx() {
                 this.visible = !this.visible;
-                if(this.visible===true){
-                    this.$nextTick(()=>{
-                        let eventHandler=()=>{
+                if (this.visible === true) {
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper);
+                        let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+                        this.$refs.contentWrapper.style.left = left + window.scrollY + 'px';
+                        let eventHandler = () => {
                             this.visible = false;
-                            document.removeEventListener('click',eventHandler)
+                            document.removeEventListener('click', eventHandler)
                         };
-                        document.addEventListener('click',eventHandler)
+                        document.addEventListener('click', eventHandler)
                     })
                 }
             }
-        }
+        },
     }
 </script>
 
 <style lang='less' scoped>
-.popover{
-    display: inline-block;
-    vertical-align: top;
-    position: relative;
-    .content-wrapper{
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgb(0,0,0,0.5);
-        position: absolute;
-        bottom: 100%;
-        left: 0;
+    .popover {
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
     }
-}
+
+    .content-wrapper {
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgb(0, 0, 0, 0.5);
+        position: absolute;
+        transform: translateY(-100%);
+    }
 </style>
