d979bbee7b09dfbfc660f570e255c5aa845c4cd6
Storm4542 <417002107@qq.com>
2018-08-06 14:01:07 +0800

    组件添加至 index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
+import Toast from './src/toast'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapse-item'
+import Tabs from './src/tabs'
+import TabsHead from './src/tabs-head'
+import TabsItem from './src/tabs-item'
+import TabBody from './src/tabs-body'
+import TabPane from './src/tabs-pane'
+import Layout from './src/layout'
+import Header from './src/header'
+import Content from './src/content'
+import Footer from './src/footer'
+import Row from './src/row'
+import Col from './src/col'
+import Popover from './src/popover'
 
-export {Button,ButtonGroup,Icon}
+
+export {Button,ButtonGroup,Icon,Toast,CollapseItem,
+    Collapse,TabsItem,Tabs,TabBody,TabPane,TabsHead,Layout,Content,
+    Footer,Header,Row,Col,Popover}
